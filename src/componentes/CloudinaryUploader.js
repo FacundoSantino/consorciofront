74561884762207f38a30573d205eac32f299ae04
FacundoSantino <sanchezfacundosantino@gmail.com>
2022-12-04 17:39:40 -0300

    ADD func

---
+++ src/componentes/CloudinaryUploader.js
@@ -0,0 +1,90 @@
+import { useState } from "react";
+import '../componentes/CloudinaryUploader.css';
+//import cloudinary from "cloudinary/lib/cloudinary"
+
+
+
+function CloudinaryUploader() {
+
+    const [images, setImages] = useState([]);
+    const [imagesToRemove, setImagesToRemove] = useState(null);
+    
+
+    
+    
+
+    function handleRemoveImg(imgObj) {
+        /*async function borrar(id) {
+            const cloudinary = require('cloudinary').v2;
+            const express = require('express');
+            const navegador = express();
+        
+            cloudinary.config({
+                cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+                api_key: process.env.API_KEY,
+                api_secret: process.env.API_SECRET
+            });
+
+            navegador.delete (async (req, res) => {
+                await cloudinary.uploader.destroy(id);}
+                
+                
+
+            )
+            
+        }*/
+        
+        
+
+        setImagesToRemove(imgObj.public_id);
+        //aca agregar el delete de la BD
+        //borrar(imgObj.public_id);
+        setImagesToRemove(null);
+        setImages((prev) => prev.filter( (img) => img.public_id !== imgObj.public_id));
+        console.log("se debería borrar");
+
+        
+        
+        
+       
+            
+        
+
+    }
+
+    function handleOpenWidget() {
+        var myWidget = window.cloudinary.createUploadWidget({
+            cloudName: 'dxpa2hk8o', 
+            uploadPreset: 'arpfatwk'}
+            , 
+            (error, result) => { 
+              if (!error && result && result.event === "success") { 
+                //console.log('Done! Here is the image info: ', result.info);
+                setImages((prev) => [...prev, {url: result.info.url, public_id: result.info.public_id}])
+              }
+            }
+          );
+        myWidget.open();
+    }
+
+    return (
+        <div className="uploader">
+            <button id= "upload-widget-cloudinary" className="cloudinary-button" onClick={() => handleOpenWidget()}>
+                Subir imagen
+            </button>
+            <div className="images-preview-caja" id="cajaPreview">
+                {images.map((image) => (
+                    <div className="image-preview" id="imagenPreview">
+                        <img src = {image.url}/>
+                        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
+                        {imagesToRemove != image.public_id && <i class="fa fa-times close-icon" onClick={() => handleRemoveImg(image)}>
+                        </i>}
+                    </div>
+                ))}
+            </div>
+
+        </div>
+    );
+}
+
+export default CloudinaryUploader; 
--- src/pages/GenerarReclamo.js
+++ src/pages/GenerarReclamo.js
@@ -1,119 +1,307 @@
-import React, {Component} from 'react'
+import React,{Component,useState, useEffect, useCallback} from 'react'
+
+
 import reclamo from '../imagenes/generarReclamo100.png'
 import logo from '../imagenes/logo.png'
 import flecha from '../imagenes/flecha.png'
 import Select from 'react-select'
+import CloudinaryUploader from '../componentes/CloudinaryUploader'
+import {Link} from 'react-router-dom'
 
 
-class GenerarReclamo extends Component{
 
-    
-    constructor(){
-        super();
-        
+export default function GenerarReclamo(){
 
-        async function getPosibilidades(){
-            const response= await fetch('http://localhost:8080/api/reclamos/consultar/?documento='+localStorage.getItem("documento"));
-            const data=await response.json();
-            alert(data.text());
-            return data;
-        }
+    const [opcionesEdificioElegido,setOpcionesEdificioElegido]= useState("none");
+    const [opcionesUnidadPisoElegido, setOpcionesUnidadPisoElegido] = useState("");
 
-        let data= getPosibilidades();
-        
-    }
-
-    volver =e =>{
-        if(localStorage.getItem("admin")=="true"){
-            window.location.pathname="/reclamosadmin"
-        }
-        else{
-            window.location.pathname="/bienvenidausuario"
-        }
-    }
-    
-
-    render(){
 
 
     
+    const [opcionesTodos, setOpcionesTodos] = useState(
+        [{label:'-',value:'-',}])
+
+    const [obtuveEdificios,setObtuveEdificios] = useState(false);
+    const [optionsEdificio, setOptionsEdificio] = useState([
+        {label:'Cargando',value:'Cargando'},
+    ])
+
+    const listadoEdificios = [];
+    const [unidadPisoPorEdificio, setUnidadPisoPorEdificio] = useState([[{label: '----', value: '----'}]]);
+
+    const usuario = localStorage.getItem("user");
+    const documentoUser = localStorage.getItem("documento");
+        let pisosEdificio = [
+            {label:'cargando', value:'cargando', edificio:'numero',piso:'numero'}
+        ];
         
-        const optionsEdificio = [
-            {label:'Edificio uno',value:'edificiouno'},
-            {label:'Edificio dos',value:'edificiodos'},
-            {label:'Edificio tres',value:'edificiotres'}
-        ]
-        const optionsPiso = [
-            {label:'1',value:'1'},
-            {label:'2',value:'2'},
-            {label:'3',value:'3'}
-        ]
-        const optionsNumero= [
-            {label:'1',value:'1'},
-            {label:'2',value:'2'},
-            {label:'3',value:'3'}
-        ]
+        
+        async function ObtenerEdificios() {
+            if(!obtuveEdificios) {
+                setObtuveEdificios(true);
+                let url = "http://localhost:8080/api/reclamos/consultar?documento="
+                await fetch(url+documentoUser).then((datos)=> {return datos.json();})
+                .then ((datos) =>{
+                for (const key in datos) {
+                    
+                    listadoEdificios.push(key);
+
+                }
+                
+           
+                
+                /*console.log(listadoEdificios);
+                console.log(datos);
+                console.log((datos["1"]));
+                console.log((datos["1"]).length)
+                
+                console.log((datos["1"])[0]);
+                
+        
+                console.log((datos["1"])[0].piso);*/
+
+                let i=0;
+                let j=0
+                let arrayAux = [ {
+                    "label" : "----------------",
+                    "value" : "",
+                    
+
+                }];
+                while (i <=listadoEdificios.length-1) {
+                    let numAux = String(listadoEdificios[i]);
+                    //let valueAux = String( "Edificio: "+listadoEdificios[i]);
+                    var aux = {
+                        "label" : numAux,
+                        "value" : "&codigoEdificio="+listadoEdificios[i]
+                    };
+                    optionsEdificio.push(aux);
+                    
+                    
+                    while (j <=(datos[numAux]).length-1) {
+                        //console.log("edificio" + listadoEdificios[i]);
+                        //console.log("piso" + (datos[numAux])[j].piso);
+                        //console.log("numero" + (datos[numAux])[j].numero);
+                
+                        //console.log(valueAux);
+                        
+
+                        let valueAux2 = String("Numero: "+((datos[numAux])[j].piso)+", Piso: "+((datos[numAux])[j].numero))
+
+                        
+                        var aux2 = {
+                            "label" : valueAux2,
+                            "value" : "&codigoUnidad="+(datos[numAux])[j].id
+                        }
+
+                        
+                        
+                        arrayAux.push(aux2);
+                        /*console.log(aux);
+                        console.log(aux2);*/
+                        
+
+
+                        j=j+1
+                    }
+                    
+
+                    unidadPisoPorEdificio.push(arrayAux);
+        
+                
+                    arrayAux = [ {
+                        "label" : "----------------",
+                        "value" : "",
+                    
+    
+                    }];
+                    i= i+1;
+                    j= 0;
+                    //console.log(unidadPisoPorEdificio);
+                }
+                optionsEdificio.shift();
+                unidadPisoPorEdificio.shift();
+
+                setOptionsEdificio(optionsEdificio);
+                setUnidadPisoPorEdificio(unidadPisoPorEdificio);
+                //console.log(unidadPisoPorEdificio);
+                
+            })
+            
+            }
+
+        }
+        ObtenerEdificios();
+        //console.log(optionsEdificio);
+        //console.log(unidadPisoPorEdificio);
+        
+        //console.log(listadoEdificios);
+        
+        //console.log(unidadPisoPorEdificio);
+    
+/*
+        for (let i=0; i<listadoEdificios.length;i++) {
+            optionsEdificio.push(listadoEdificios[i]);
+        } */
+
+        function handleChange(e) {
+            
+            
+            
+
+            //document.getElementById("pisoUnidad").value = unidadPisoPorEdificio[0][0];
+        
+            //console.log(e.label);
+            //console.log(e.value);
+            setOpcionesEdificioElegido(String(e.value));
+            
+            //console.log(optionsEdificio.leng);
+
+            let auxA = e.label;
+            
+            //console.log(optionsEdificio[0].label);
+            
+            for (let k = 0; k <= optionsEdificio.length-1;k++) {
+                let auxB = optionsEdificio[k].label;
+                if (auxA == auxB) {
+                    //console.log("exito" + auxB);
+                    //console.log(unidadPisoPorEdificio[k][0]);
+                    //console.log(unidadPisoPorEdificio)
+                    setOpcionesTodos(unidadPisoPorEdificio[k]);
+                    
+                    //console.log(document.getElementById("selectUnidadPiso"));
+                    
+                
+                }
+            }
+            
+            
+            //console.log(opcionesTodos[0]);
+            //console.log(parseInt(e.label));
+
+        }
+
+        function handleChangeUnidadPiso(e) {
+            //console.log(e.label);
+            //console.log(e.value);
+            
+            setOpcionesUnidadPisoElegido(String(e.value));
+            
+        }
+
+        async function generadorReclamo() {
+            /*console.log(opcionesEdificioElegido);
+            console.log(opcionesUnidadPisoElegido);
+            console.log(document.getElementById("ubicacion").value);
+            console.log(document.getElementById("descripcion").value);*/
+            let url2 = 'http://localhost:8080/api/reclamos?documento='+documentoUser+
+            '&usuario='+usuario+String(opcionesEdificioElegido)+String(opcionesUnidadPisoElegido)+
+            '&ubicacion='+String(document.getElementById("ubicacion").value)+
+            '&descripcion='+String(document.getElementById("descripcion").value);
+
+          
+            await fetch(url2,{method : 'POST'}).then((datos)=> {return datos.json();})
+            .then((datos) => {
+                document.getElementById("textoAuxGenerador").innerHTML = "Se ha generado el reclamo con exito con el id de reclamo:"+datos.idReclamo;
+                imagenGenerador(datos.idReclamo);
+            })
+           
+
+            
+
+            
+
+        }
+        
+        async function imagenGenerador(numeroReclamo) {
+            //console.log(document.getElementById("imagenCloudinary").querySelector("#imagenPreview").getElementsByTagName("img")[0].src);
+            //console.log(document.getElementById("imagenCloudinary").querySelector("#cajaPreview").getElementsByTagName("div")[1].getElementsByTagName("img")[0].src)
+            let cantidadImg =   document.getElementById("imagenCloudinary").querySelector("#cajaPreview").getElementsByTagName("div").length;
+            let url3 = 'http://localhost:8080/api/imagenes?tipo=png&idReclamo='+String(numeroReclamo)+"&usuario="+usuario;
+            let srcImagenAux = "";
+            for (let n= 0; n<=cantidadImg-1; n++) {
+                srcImagenAux = document.getElementById("imagenCloudinary").querySelector("#cajaPreview").getElementsByTagName("div")[n].getElementsByTagName("img")[0].src;
+
+                await fetch((url3+"&path="+srcImagenAux),{method: 'POST'});
+                //document.getElementById("textoAuxGenerador").innerHTML = document.getElementById("textoAuxGenerador").innerHTML + "<pre>"+ "Se ha subido la imagen "+(n+1)+" con exito" +"</pre>";
+
+                
+
+            }
+        }
+    
+       
+        
+  
+       
+
           
         return(
             
             
             <main className='clase'>
-                <div className='caja'>
+                <div id="caja" className='caja'>
                     <div className='tituloLogoPerfil'>
-                        <img src={logo}/>
-                        <h1>Generar Reclamo</h1>
-                        <img src={reclamo}/>
-                        <button className='boton' onClick={this.volver}> <img src={flecha}/> </button> 
+                        <img src={logo} className='item'/>
+                        <h1 className='item'>Generar Reclamo</h1 >
+                        <img src={reclamo} className='item'/>
+                        <Link id="volver" className='boton item' to={localStorage.getItem("admin")=="true" ?'/bienvenidaadmin' : '/bienvenidausuario'}> <img src={flecha}/> </Link> 
                     </div>
                     <h2 className='datos'> DATOS DEL EDIFICIO </h2>
-                    <div className='datosEdificio'>
+                    <div className='datosEdificio' >
                         <div id="edificio" className='drop'>
                             <span>Edificio</span>
-                            <Select 
+                            <form >
+                            <Select  id='selectEdificio'
+                                onChange={e => handleChange(e)} 
                                 options={optionsEdificio} 
                                 placeholder="Elegí un edificio" 
                                 className="basic-single"
                                 classNamePrefix="select"
                                 name="Edificio"
                             />
+
+                            </form>
+                            
                         </div>
-                        <div id="piso"className='drop'>
-                            <span>Piso</span>
-                            <Select 
-                                options={optionsPiso} 
-                                placeholder="Elegí un piso" 
+                        <div id="unidadPiso" className='drop'>
+                            <span>Unidad y Piso</span>
+                            <form  >
+                            <Select name='selectUnidadPiso' id="selectUnidadPiso"
+                                onChange={e => handleChangeUnidadPiso(e)} 
+                                options={opcionesTodos} 
+                                placeholder="Elegí una unidad y piso" 
                                 className="basic-single"
                                 classNamePrefix="select"
-                                name="Piso"
+                                
+                            
+                                
                             />
+
+                            </form>
+                            
                         </div>
-                        <div id="numero" className='drop'>
-                            <span>Número</span>
-                            <Select 
-                                options={optionsNumero} 
-                                placeholder="Elegí un número" 
-                                className="basic-single"
-                                classNamePrefix="select"
-                                name="Numero"
-                            />
-                        </div>
+                        
                     </div>
                     <h2 className='proble'> PROBLEMÁTICA </h2>
                     <div className='problematica'>
                         <div className='drop'>
                             <span>Ubicación</span>
-                            <textarea type="text"></textarea>
+                            <textarea id='ubicacion' type="text"></textarea>
                         </div>
                         <div className='drop'>
                             <span>Descripción</span>
-                            <textarea name="textarea"></textarea>
+                            <textarea id='descripcion' name="textarea"></textarea>
                         </div>
-                        <div className='imagenes'>
+                        <div className='imagenes' id='imagenCloudinary'>
                             <span>Imágenes</span>
-                            <input type="file" multiple/>
+                            <CloudinaryUploader/>
                         </div>
                     </div>
-                    <button className='boton'>Generar</button>
+                    <button className='gen boton' onClick={() => generadorReclamo()}>Generar</button>
+                    <p id='textoAuxGenerador'>
+
+                    </p>
 
 
                 </div>
@@ -122,7 +310,5 @@ class GenerarReclamo extends Component{
             
         );
     }
-}
 
 
-export default GenerarReclamo;
